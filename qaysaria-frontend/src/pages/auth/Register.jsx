658cede55774a97a6f46b066aeb20ca5b822980b
unknown <redaelksis@gmail.com>
2026-03-17 22:25:35 +0100

    corriger issues build:vercel

--- qaysaria-frontend/src/pages/auth/Register.jsx
+++ qaysaria-frontend/src/pages/auth/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../../styles//pages css//auth.css";
 
 function Register() {
