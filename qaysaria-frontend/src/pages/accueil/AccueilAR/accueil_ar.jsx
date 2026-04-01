cd105b057f6bab80bfcd18004c76183f0e1a73bb
wissal <wissaloukili56@gmail.com>
2026-04-01 22:09:51 +0100

    Fix header design

--- qaysaria-frontend/src/pages/accueil/AccueilAR/accueil_ar.jsx
+++ qaysaria-frontend/src/pages/accueil/AccueilAR/accueil_ar.jsx
@@ -133,7 +133,7 @@ const AccueilAR = () => {
   );
 
   return (
-    <div dir="rtl" lang="ar">
+    <div dir="rtl" class="ar-home" lang="ar">
 
       {/* ════ HERO ════ */}
       <section className="ar-hero-section">
